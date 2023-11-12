23f77183c2f18bdc497c3f07187858e66467b367
Daniel Zhang <danielzhang@Daniels-MacBook-Pro.local>
2023-11-13 11:06:18 +1300

    sending the correct informaiton now

--- src/Controller/routes.php
+++ src/Controller/routes.php
@@ -26,7 +26,7 @@ class routes extends AbstractController
                 $filterOptionsJSON = $request->getContent();
                 $decodedOptions = json_decode($filterOptionsJSON, true);
                 if($decodedOptions && $decodedOptions['generateReport'] && $decodedOptions['generateReport'] === true) {
-                    $res = $this->generateRevenueStatement();
+                    $res = $this->generateRevenueStatement($decodedOptions);
                 } else if (!$decodedOptions || !$decodedOptions['groupBy']) {
                     return new Response('no');
                 } else {
@@ -149,8 +149,101 @@ class routes extends AbstractController
     }
 
     private function generateRevenueStatement($decodedOptions = null) {
+        $startDate = $decodedOptions['startDate'];
+        $endDate = $decodedOptions['endDate'];
+        $res = [];
 
-        return 'working';
+        // Fetch data using JOIN to retrieve information from multiple tables
+        $stmt = $this->pdo->prepare('
+        SELECT
+            s.student_id,
+            s.student_first_name,
+            s.student_surname,
+            e.enrolment_number,
+            e.date AS enrolment_date,
+            ih.invoice_header_number,
+            ih.amount_due,
+            ih.due_date,
+            il.invoice_line_number,
+            il.payment_date,
+            il.amount AS line_item_amount
+        FROM student s
+        LEFT JOIN enrolment e ON s.student_id = e.student_id
+        LEFT JOIN invoice_header ih ON e.enrolment_number = ih.enrolment_number
+        LEFT JOIN invoice_line il ON ih.invoice_header_number = il.invoice_header_number
+        WHERE ih.due_date >= :start_date AND ih.due_date <= :end_date
+    ');
+
+        $stmt->bindParam(':start_date', $startDate, \PDO::PARAM_STR);
+        $stmt->bindParam(':end_date', $endDate, \PDO::PARAM_STR);
+        $stmt->execute();
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($data as $row) {
+            $studentId = $row['student_id'];
+
+            if (!isset($res[$studentId])) {
+                $res[$studentId]['id'] = $studentId;
+                $res[$studentId]['firstname'] = $row['student_first_name'];
+                $res[$studentId]['surname'] = $row['student_surname'];
+                $res[$studentId]['enrolments'] = [];
+            }
+
+            $enrolmentNumber = $row['enrolment_number'];
+
+            if (!isset($res[$studentId]['enrolments'][$enrolmentNumber])) {
+                $res[$studentId]['enrolments'][$enrolmentNumber]['enrolment_number'] = $enrolmentNumber;
+                $res[$studentId]['enrolments'][$enrolmentNumber]['enrolment_date'] = $row['enrolment_date'];
+                $res[$studentId]['enrolments'][$enrolmentNumber]['invoice_headers'] = [];
+            }
+
+            $headerNumber = $row['invoice_header_number'];
+
+            if (!isset($res[$studentId]['enrolments'][$enrolmentNumber]['invoice_headers'][$headerNumber])) {
+                $res[$studentId]['enrolments'][$enrolmentNumber]['invoice_headers'][$headerNumber]['header_number'] = $headerNumber;
+                $res[$studentId]['enrolments'][$enrolmentNumber]['invoice_headers'][$headerNumber]['amount'] = $row['amount_due'];
+                $res[$studentId]['enrolments'][$enrolmentNumber]['invoice_headers'][$headerNumber]['balance'] = $row['amount_due'];
+                $res[$studentId]['enrolments'][$enrolmentNumber]['invoice_headers'][$headerNumber]['line_items'] = [];
+            }
+
+            $lineItemNumber = $row['invoice_line_number'];
+
+            if (!isset($res[$studentId]['enrolments'][$enrolmentNumber]['invoice_headers'][$headerNumber]['line_items'][$lineItemNumber])) {
+                $res[$studentId]['enrolments'][$enrolmentNumber]['invoice_headers'][$headerNumber]['line_items'][$lineItemNumber]['invoice_line_number'] = $lineItemNumber;
+                $res[$studentId]['enrolments'][$enrolmentNumber]['invoice_headers'][$headerNumber]['line_items'][$lineItemNumber]['payment_date'] = $row['payment_date'];
+                $res[$studentId]['enrolments'][$enrolmentNumber]['invoice_headers'][$headerNumber]['line_items'][$lineItemNumber]['amount'] = $row['line_item_amount'];
+            }
+
+            // Subtract line item amount from the balance
+            $res[$studentId]['enrolments'][$enrolmentNumber]['invoice_headers'][$headerNumber]['balance'] -= $row['line_item_amount'];
+        }
+
+        return array_values($res);
     }
 
+
+
 }
+
+
+
+//{
+//    "$sid": {
+//    "name": {
+//        "firstname": "$firstname",
+//    "surname": "$surname",
+//    "id": "$id"
+//  },
+//  "$enrolment_number": {
+//        "$header_number": {
+//            "amount": "$amount_due",
+//      "balance": "$to be determined",
+//      "$line_items": {
+//                "date": "$date",
+//        "amount": "$amount"
+//      }
+//    },
+//    "enrolment_date": "$enrolment_date"
+//  }
+// }
+//}
